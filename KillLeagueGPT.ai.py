ee025ae87f6ffe5cd785f987727f1e445dcf8895
Tyler Patten <tylerpattenvideo@gmail.com>
2023-08-13 01:02:18 -0400

    Made it eternal

--- KillLeagueGPT.ai.py
+++ KillLeagueGPT.ai.py
@@ -1,7 +1,8 @@
 import os
 
 def main():
-    if any('LeagueClient.exe' in s for s in os.popen('tasklist').readlines()):
-        os.system('taskkill /f /im LeagueClient.exe')
+    while True:
+        if any('LeagueClient.exe' in s for s in os.popen('tasklist').readlines()):
+            os.system('taskkill /f /im LeagueClient.exe')
 
 main()
